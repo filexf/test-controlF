314e008acb4d3fcfbe287c93fe763c71afa68514
Félix Orain <felix.orain@gmail.com>
2025-07-10 02:05:13 +0200

    Refactor 🔧 - Simplified RetourAccueilButton component by removing unnecessary onNavigate prop and directly using navigate function for improved clarity.

--- src/components/ui/RetourAccueilButton.jsx
+++ src/components/ui/RetourAccueilButton.jsx
@@ -2,14 +2,12 @@ import CustomButton from "@/components/ui/CustomButton";
 import { House } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
-export default function RetourAccueilButton({ onNavigate, className = "" }) {
+export default function RetourAccueilButton({ className = "" }) {
   const navigate = useNavigate();
   return (
     <CustomButton
       className={`w-full sm:w-auto flex items-center gap-2 justify-center shadow-lg ${className}`}
-      onClick={() =>
-        typeof onNavigate === "function" ? onNavigate() : navigate("/")
-      }
+      onClick={() => navigate("/")}
     >
       <House />
       Retour à l'accueil
